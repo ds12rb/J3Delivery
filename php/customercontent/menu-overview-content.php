e5c40cab735323bbd0dc526570d72eaaae2a86d4
Danny <ds12rb@brocku.ca>
2016-02-16 22:09:30 -0500

    shopping cart working decent

--- php/customercontent/menu-overview-content.php
+++ php/customercontent/menu-overview-content.php
@@ -1,8 +1,9 @@
 <div id="menu-overview-content-container" class="container">
 
-  <?php require("../../php/customercontent/shoppingcart.php"); ?>
   <?php require("../../php/customercontent/restaurant-header.php"); ?>
   <?php require("../../php/customercontent/menu-specials.php"); ?>
   <?php require("../../php/customercontent/menu-appetizers.php"); ?>
   <?php require("../../php/customercontent/menu-mains.php"); ?>
+  <?php require("../../php/customercontent/shoppingcart.php"); ?>
+
 </div>
